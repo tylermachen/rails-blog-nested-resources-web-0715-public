88bb2c3a4d618a92361b9acce8ac6eaf1d7fac92
Tyler Machen <tylermachen@gmail.com>
2015-09-03 15:58:04 -0400

    remove comments in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,4 @@ class User < ActiveRecord::Base
 
   validates_uniqueness_of :name
   validates_presence_of :name
-  # after_destroy :remove_comments
-
-  # private
-  #
-  # def remove_comments
-  #   comments.delete_all
-  # end
 end
